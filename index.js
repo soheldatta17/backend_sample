7733ef451e9447cb98151299cbe75cf50872730f
soheldatta17 <soheldatta17@gmail.com>
2024-07-03 13:13:18 +0530

    .

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
+// List of football facts
+const footballFacts = [
+    "Football is also known as soccer in some parts of the world.",
+    "The first football club was formed in 1857, called Sheffield FC.",
+    "The FIFA World Cup is the most-watched sporting event in the world.",
+    "Pele is the only player to have won three FIFA World Cups.",
+    "The fastest goal in World Cup history was scored by Hakan Şükür in 11 seconds.",
+    "Lionel Messi and Cristiano Ronaldo are considered two of the greatest players of all time.",
+    "The 2022 FIFA World Cup was held in Qatar.",
+    "The Premier League is the most-watched football league globally.",
+    "The term 'hat-trick' refers to a player scoring three goals in a single game.",
+    "The first official football rules were codified by the Cambridge Rules in 1848."
+];
+
 // Route to handle GET requests at '/'
 app.get('/', (req, res) => {
     res.send('Hi this is a back-end server deployed by Sohel to save the form details. You need an API key to continue...!');
@@ -15,6 +29,11 @@ app.get('/', (req, res) => {
 // Route to handle GET requests at '/hello'
 app.get('/hello', (req, res) => {
     res.send('hello world');
+});
+
+// Route to handle GET requests at '/facts'
+app.get('/facts', (req, res) => {
+    res.json(footballFacts);
 });
 
 // Start the server
